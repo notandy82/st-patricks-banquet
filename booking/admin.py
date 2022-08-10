0324ac389431ce3ba527d2c01cfff732a210f19c
notandy82 <notandystanek@gmail.com>
2022-08-10 16:18:39 +0000

    Add ability to see total of each meal in admin panel

--- booking/admin.py
+++ booking/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 from .models import Booking, Post
+from admin_totals.admin import ModelAdminTotals
+from django.db.models import Sum
 
-admin.site.register(Booking)
+# admin.site.register(Booking)
 admin.site.register(Post)
+@admin.register(Booking)
 
 
-class BookingAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ("booking_number",)}
-
-
+class BookingAdmin(ModelAdminTotals):
+    list_display = (
+        "booking_number",
+        "adult_meat",
+        "adult_vegetarian",
+        "children",
+        )
+    list_totals = [("adult_meat", Sum), ("adult_vegetarian", Sum), ("children", Sum)]
--- nisbanquet/settings.py
+++ nisbanquet/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'crispy_forms',
+    'admin_totals',
     'booking',
 ]
 
@@ -147,14 +148,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
+STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 
 
 # Default primary key field type
